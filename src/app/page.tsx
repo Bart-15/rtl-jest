c27d0e6b3b8b8dab73262298cc149519aa184d8c
Bart-15 <brat.tabusao@gmail.com>
2025-05-08 17:52:51 +0800

    revert root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
-import NewsletterSignup from '@/components/counter/news-letter-signup';
 import TodoList from '@/components/todo/Todo';
 
 export default function Home() {
   return (
     <main className="flex p-24">
-      {/* <TodoList /> */}
-      <NewsletterSignup />
+      <TodoList />
     </main>
   );
 }
